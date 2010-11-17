2494eb96376524c812c5850ebc9f7b599578aa5e
unknown <rayan@.ant.amazon.com>
2010-11-17 11:56:24 +0530

    Crude solution to problem3

--- src/projecteuler/problem3.clj
+++ src/projecteuler/problem3.clj
@@ -1,8 +1,4 @@
 (ns projecteuler.problem3)
-(defn something? [n]
-  (do (def y 10) (println y) (> y 11)))
-
-(println (something? 1))
 (def primes `(2))
 (defn is-prime? [n]
   (do 
@@ -14,6 +10,32 @@
       (def primes (cons n primes)))
     isp
     ))
+(defn max-prime [n]
+  (last
+    (filter #(zero? (mod n %))
+        (filter is-prime? 
+          (range 2 (Math/sqrt n))))))
 
-(println (lazy-seq (for [i (range 2 100)] [i (is-prime? i)])))
-
+(def n (inc (int 
+  (Math/sqrt 600851475143))))
+(println n)
+(def isprime (boolean-array n true))
+(defn set-primes [prime n]
+  (loop [m 2]
+    (if (<= (* m prime) n)
+      (do (aset isprime (* m prime) false)
+      (recur  (inc m))))))
+(def ans 2)
+(defn find-prime [] 
+  (loop [p 2]
+    (if (< p n)
+      (do
+        (if (aget isprime p)
+          (do (if (zero? (mod 600851475143 p)) (def ans p)) (println (str "Prime:" ans))
+            (set-primes p n)))
+        (recur (inc p))))))
+    
+(find-prime)
+(println (ans)
+      
+    
