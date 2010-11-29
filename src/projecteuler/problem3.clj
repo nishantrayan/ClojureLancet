1777ab31027b498947f57eac8aee56c643151638
U-ANT\rayan <rayan@IN-MAA-R8WNRZW.ant.amazon.com>
2010-11-29 14:16:51 +0530

    Cleaned up some problem solutions and moved to idea :)

--- src/projecteuler/problem3.clj
+++ src/projecteuler/problem3.clj
@@ -1,24 +1,6 @@
 (ns projecteuler.problem3)
-(def primes `(2))
-(defn is-prime? [n]
-  (do 
-    (def isp 
-      (not-any? #(zero? (mod n %))
-        (filter #(<= % (inc (Math/sqrt n)))
-          primes)))
-    (if isp
-      (def primes (cons n primes)))
-    isp
-    ))
-(defn max-prime [n]
-  (last
-    (filter #(zero? (mod n %))
-        (filter is-prime? 
-          (range 2 (Math/sqrt n))))))
-
 (def n (inc (int 
   (Math/sqrt 600851475143))))
-(println n)
 (def isprime (boolean-array n true))
 (defn set-primes [prime n]
   (loop [m 2]
@@ -36,6 +18,6 @@
         (recur (inc p))))))
     
 (find-prime)
-(println (ans)
+
       
     
--- src/puzzlemaster/puzzle1.clj
+++ src/puzzlemaster/puzzle1.clj
@@ -1,14 +1,18 @@
 (ns projecteuler.puzzle1)
+(require 'clojure.string)
 (defn div? [n d]
   (zero? (mod n d)))
 (defn map-num [n]
   (if(div? n 15) "Hop"
     (if(div? n 5) "Hophop"
       (if(div? n 3) "Hoppity"))))
-(println (apply str  
-           (interpose "\n" 
-             (filter (complement nil?) 
-               (map map-num (range 10))))))
+(def n 
+    (line-seq (java.io.BufferedReader. *in*)))
+(println n)
+;(println (apply str  
+ ;          (interpose "\n" 
+ ;            (filter (complement nil?) 
+  ;             (map map-num (range n))))))
 
 
     
