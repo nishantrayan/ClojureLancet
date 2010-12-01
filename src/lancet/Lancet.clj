cd57c71899954e4d578b57de74b99b9b1cb61e81
U-ANT\rayan <rayan@IN-MAA-R8WNRZW.ant.amazon.com>
2010-12-01 10:34:23 +0530

    added set-properties to set multiple properties at a time

--- src/lancet/Lancet.clj
+++ src/lancet/Lancet.clj
@@ -27,8 +27,14 @@
 (defn set-property! [inst prop value]
   (let [pd (property-descriptor inst prop)]
     (.invoke (.getWriteMethod pd) inst (into-array [value]))))
+;extending to support setting multiple properties
+(defn set-properties! [inst values]
+  (doseq [[k v] values] (set-property! inst k v)))
 ;before set-property
 (.execute echo-task)
 (set-property! echo-task :message "this was changed later using setProperty function")
 ;after set-property
+(.execute echo-task)
+;using set-properties!
+(set-properties! echo-task {:message "this was set using set-properties"})
 (.execute echo-task)
