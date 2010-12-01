57c2b97269bd0fa3d4da7ef396094a387a18f7f1
U-ANT\rayan <rayan@IN-MAA-R8WNRZW.ant.amazon.com>
2010-12-01 09:36:36 +0530

    simple get functions on map

--- src/tryout/MapTryouts.clj
+++ src/tryout/MapTryouts.clj
@@ -3,3 +3,9 @@
 (def map2 {:n1 0 :n2 12})
 (def merged (merge-with choose-gt map1 map2))
 (println merged)
+; key as function
+(println (:n1 merged))
+; map as function
+(println (merged :n1))
+; value not present
+(println (merged :n3 100))
